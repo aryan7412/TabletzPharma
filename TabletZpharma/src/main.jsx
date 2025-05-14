44461d2c0b4ae2f3a937848bbd7c297c7de7f94b
aryan7412 <aryan7412ss@gmail.com>
2025-05-14 22:21:44 +0530

    deploy2

--- TabletZpharma/src/main.jsx
+++ TabletZpharma/src/main.jsx
@@ -14,12 +14,8 @@ const router = createBrowserRouter(
       <Route path="contact" element={<Contact/>} />
       <Route path="products" element={<Products/>} />
       <Route path="products/:category" element={<Products/>} />
-      <Route path="*" element={<Home/>} />
     </Route>
-  ),
-  {
-    basename: '/'
-  }
+  )
 )
 
 createRoot(document.getElementById('root')).render(
