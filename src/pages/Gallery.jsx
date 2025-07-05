3cfda3c1d88e8d4ab851739e20e0cebbb240db9c
aryan7412 <aryan7412ss@gmail.com>
2025-07-05 20:48:58 +0530

    Solved an gallery Issue of animation

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -15,8 +15,8 @@ function Gallery() {
       {/* Heading */}
       <div className='text-4xl sm:text-5xl font-bold text-center my-5' data-aos="fade-up">GALLERY</div>
       {/* Tagline */}
-      <div className='text-center'>Images</div>
-      <hr className='my-5 mx-auto w-2/3 sm:w-1/2 md:w-1/3 border-black border-[1px]' />
+      <div className='text-center'data-aos="fade-up">Images</div>
+      <hr className='my-5 mx-auto w-2/3 sm:w-1/2 md:w-1/3 border-black border-[1px]' data-aos="fade-up"/>
       
       {/* Gallery Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 px-1 sm:px-8 md:px-16 lg:px-24 max-w-md sm:max-w-4xl mx-auto">
